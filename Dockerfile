beb594fcb25d09e6c3f051029e2612ca4ac7c714
root <root@docker-ansible.anovaes.net>
2019-02-17 03:57:19 +0000

    ajuste no dockerfile e no scrtip de inicialização

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,10 @@ RUN yum -y update; yum -y install openssh-server passwd bind bind-utils vim ipro
 RUN mkdir -p /var/run/sshd && ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' && rndc-confgen >> /etc/rndc.conf
 EXPOSE 22/tcp 53/udp
 ADD start-ssh-dns.sh /opt/start-ssh-dns.sh
-ADD named.conf /etc/named.conf
+#ADD named.conf /etc/named.conf
 EXPOSE 22/tcp 53/udp
 ENTRYPOINT [ "/opt/start-ssh-dns.sh" ]
 
-/
 
-https://access.redhat.com/solutions/54644
-http://linux-n-linux.blogspot.com/2012/10/resolve-rndc-error-in-rhel6-centos6.html
+#https://access.redhat.com/solutions/54644
+#http://linux-n-linux.blogspot.com/2012/10/resolve-rndc-error-in-rhel6-centos6.html
--- start-ssh-dns.sh
+++ start-ssh-dns.sh
@@ -3,18 +3,23 @@
 #Usar variável de ambiente para passar usuário e senha (secret)
 
 #SSH Server
-useradd ansible
-SSH_USERPASS="olamundo"
-echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin ansible)
+#Variável USER_CUSTOM e PASSWD_CUSTOM deve ser informado no momento do boot do container
+useradd $USER_CUSTOM
+SSH_USERPASS="$PASSWD_CUSTOM"
+echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin USER_CUSTOM)
 
 /usr/sbin/sshd -D &
 
-IPADDRESS=$(ip route|grep 'link src'|awk '{print $9}')
-sed "s/127.0.0.1/'$IPADDRESS'/g"  /etc/rndc.conf -i
+#IPADDRESS=$(ip route|grep 'link src'|awk '{print $9}')
+#sed "s/127.0.0.1/'$IPADDRESS'/g"  /etc/rndc.conf -i
+
+CHAVE_RNDC=$(grep -i secret /etc/rndc.conf |grep -v '#'|awk '{print $2}'|cut -d\" -f2)
+
+echo "Minha key_secret do RNDC é: $CHAVE_RNDC"
 
 #Bind Server DNS
 /bin/bash -c 'if [ ! "$DISABLE_ZONE_CHECKING" == "yes" ]; then /usr/sbin/named-checkconf -z "$NAMEDCONF"; else echo "Checking of zone files is disabled"; fi'
-/usr/sbin/named -u named -c /etc/named.conf
+/usr/sbin/named -u named -c /etc/named.conf -f
 
 
 #http://jon.netdork.net/2008/08/21/bind-dynamic-zones-and-updates/
