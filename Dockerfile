f6b688b31cd73a4c8abe304ec30cf2b2402507ad
Antonio Novaes <anovaes@tjba.jus.br>
2019-02-18 18:05:11 -0300

    export volume dockergile, ajuste script de boot e removido rndc.conf de teste

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ RUN yum -y install openssh-server passwd bind bind-utils vim iproute nc net-tool
 RUN mkdir -p /var/run/sshd && ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' && rndc-confgen >> /etc/rndc.conf
 ADD start-ssh-dns.sh /opt/start-ssh-dns.sh
 ADD named.conf /etc/named.conf
+VOLUME /var/named
 ADD named.exemplo /var/named/named.exemplo
-#ADD db.exemplo.com.br /var/named/db.exemplo.com.br
 EXPOSE 22/tcp 53/udp 53/tcp
 ENTRYPOINT [ "/opt/start-ssh-dns.sh" ]
 
--- start-ssh-dns.sh
+++ start-ssh-dns.sh
@@ -10,6 +10,8 @@ SSH_USERPASS="$PASSWD_CUSTOM"
 echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin $USER_CUSTOM)
 
 
+if [ ! -f /opt/primeiro_boot_nao_delete.txt ]; then
+
 IPADDRESS=$(ip route|grep 'scope link'|awk '{print $9}')
 CHAVE_RNDC=$(grep -i secret /etc/rndc.conf |grep -v '#'|awk '{print $2}'|cut -d\" -f2)
 
@@ -27,12 +29,16 @@ cp -pav /var/named/named.exemplo /var/named/db.$DOMINIO
 sed -i "s/127.0.0.1/$IPADDRESS/g" /var/named/db.$DOMINIO
 sed -i "s/rname.invalid/root.$DOMINIO/g" /var/named/db.$DOMINIO
 
+touch /opt/primeiro_boot_nao_delete.txt
+
+fi
+
 
 
 #Bind Server DNS
 /bin/bash -c 'if [ ! "$DISABLE_ZONE_CHECKING" == "yes" ]; then /usr/sbin/named-checkconf -z "$NAMEDCONF"; else echo "Checking of zone files is disabled"; fi'
 
-
 #Iniciando serviços SSH e BIND
+
 /usr/sbin/sshd -D  &
 /usr/sbin/named -u named -c /etc/named.conf -f
