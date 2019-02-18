3ec89dc998e62e994d0bf58a0bf33021952c3298
Antonio Novaes <antonionovaesjr@gmail.com>
2019-02-18 04:10:50 +0000

    ajuste no script start e obtencao de IP

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 From centos
 RUN yum -y update; yum -y install openssh-server passwd bind bind-utils vim iproute nc net-tools python-dns.noarch; yum clean all
 RUN mkdir -p /var/run/sshd && ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' && rndc-confgen >> /etc/rndc.conf
-EXPOSE 22/tcp 53/udp
 ADD start-ssh-dns.sh /opt/start-ssh-dns.sh
-#ADD named.conf /etc/named.conf
-EXPOSE 22/tcp 53/udp
+ADD named.conf /etc/named.conf
+ADD db.exemplo.com.br /var/named/db.exemplo.com.br
+EXPOSE 22/tcp 53/udp 53/tcp
 ENTRYPOINT [ "/opt/start-ssh-dns.sh" ]
 
 
--- start-ssh-dns.sh
+++ start-ssh-dns.sh
@@ -6,22 +6,30 @@
 #Variável USER_CUSTOM e PASSWD_CUSTOM deve ser informado no momento do boot do container
 useradd $USER_CUSTOM
 SSH_USERPASS="$PASSWD_CUSTOM"
-echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin USER_CUSTOM)
+echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin $USER_CUSTOM)
 
 /usr/sbin/sshd -D &
 
-#IPADDRESS=$(ip route|grep 'link src'|awk '{print $9}')
-#sed "s/127.0.0.1/'$IPADDRESS'/g"  /etc/rndc.conf -i
+IPADDRESS=$( ip route|grep 'scope link'|awk '{print $9}')
+
+#Editando o arquivo de domínio
+mv /var/named/db.exemplo.com.br /var/named/db.$DOMINIO
+sed -i "s/127.0.0.1/$IPADDRESS/g" /var/named/db.$DOMINIO
+sed -i "s/exemplo.com.br/$DOMINIO/g" /var/named/db.$DOMINIO
+
+#Pegando o RNDC-KEY
 
 CHAVE_RNDC=$(grep -i secret /etc/rndc.conf |grep -v '#'|awk '{print $2}'|cut -d\" -f2)
-
 echo "Minha key_secret do RNDC é: $CHAVE_RNDC"
+
+#Editando o named.conf
+
+sed -i "s/127.0.0.1/$IPADDRESS/g" /etc/named.conf
+sed -i "s/127.0.0.1/$IPADDRESS/g" /etc/rndc.conf
+sed -i "s/RNDC_CHAVE/$CHAVE_RNDC/g" /etc/named.conf
+sed -i "s/exemplo.com.br/$DOMINIO/g" /etc/named.conf
 
 #Bind Server DNS
 /bin/bash -c 'if [ ! "$DISABLE_ZONE_CHECKING" == "yes" ]; then /usr/sbin/named-checkconf -z "$NAMEDCONF"; else echo "Checking of zone files is disabled"; fi'
 /usr/sbin/named -u named -c /etc/named.conf -f
 
-
-#http://jon.netdork.net/2008/08/21/bind-dynamic-zones-and-updates/
-#https://www.centos.org/forums/viewtopic.php?t=46291
-#http://www.zytrax.com/books/dns/ch7/xfer.html#allow-update
