f124370a3273b850b196f1b561546844eeb65aab
Antonio Novaes <anovaes@tjba.jus.br>
2019-02-18 17:15:01 -0300

    novo yaml ansible, ajuste no script de start

--- start-ssh-dns.sh
+++ start-ssh-dns.sh
@@ -4,32 +4,35 @@
 
 #SSH Server
 #Variável USER_CUSTOM e PASSWD_CUSTOM deve ser informado no momento do boot do container
+
 useradd $USER_CUSTOM
 SSH_USERPASS="$PASSWD_CUSTOM"
 echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin $USER_CUSTOM)
 
-/usr/sbin/sshd -D &
 
-IPADDRESS=$( ip route|grep 'scope link'|awk '{print $9}')
-
-#Editando o arquivo de domínio
-mv /var/named/db.exemplo.com.br /var/named/db.$DOMINIO
-sed -i "s/127.0.0.1/$IPADDRESS/g" /var/named/db.$DOMINIO
-sed -i "s/exemplo.com.br/$DOMINIO/g" /var/named/db.$DOMINIO
-
-#Pegando o RNDC-KEY
-
+IPADDRESS=$(ip route|grep 'scope link'|awk '{print $9}')
 CHAVE_RNDC=$(grep -i secret /etc/rndc.conf |grep -v '#'|awk '{print $2}'|cut -d\" -f2)
-echo "Minha key_secret do RNDC é: $CHAVE_RNDC"
+
+#Do ip do server que o RNDC conecta
+sed -i "s/127.0.0.1/$IPADDRESS/g" /etc/rndc.conf
 
 #Editando o named.conf
 
 sed -i "s/127.0.0.1/$IPADDRESS/g" /etc/named.conf
-sed -i "s/127.0.0.1/$IPADDRESS/g" /etc/rndc.conf
 sed -i "s/RNDC_CHAVE/$CHAVE_RNDC/g" /etc/named.conf
 sed -i "s/exemplo.com.br/$DOMINIO/g" /etc/named.conf
 
+#Editando o arquivo de domínio
+cp -pav /var/named/named.exemplo /var/named/db.$DOMINIO
+sed -i "s/127.0.0.1/$IPADDRESS/g" /var/named/db.$DOMINIO
+sed -i "s/rname.invalid/root.$DOMINIO/g" /var/named/db.$DOMINIO
+
+
+
 #Bind Server DNS
 /bin/bash -c 'if [ ! "$DISABLE_ZONE_CHECKING" == "yes" ]; then /usr/sbin/named-checkconf -z "$NAMEDCONF"; else echo "Checking of zone files is disabled"; fi'
-/usr/sbin/named -u named -c /etc/named.conf -f
 
+
+#Iniciando serviços SSH e BIND
+/usr/sbin/sshd -D  &
+/usr/sbin/named -u named -c /etc/named.conf -f
